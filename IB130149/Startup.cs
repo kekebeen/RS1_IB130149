78ac80b833905f9247a3b539e383551234ebd943
unknown <kakosi69@gmail.com>
2020-01-06 23:11:07 +0100

    Added logging on production

--- IB130149/Startup.cs
+++ IB130149/Startup.cs
@@ -30,7 +30,7 @@ namespace IB130149
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -39,7 +39,8 @@ namespace IB130149
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }
+            }*/
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
