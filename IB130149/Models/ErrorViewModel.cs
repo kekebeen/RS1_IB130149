47f61299cbb173cf184d07e733dcfcd450922343
unknown <kakosi69@gmail.com>
2020-01-06 19:44:14 +0100

    initial commit

---
+++ IB130149/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IB130149.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
