2dc7a76e28233691d1503e778f2000a43644d7ff
unknown <kakosi69@gmail.com>
2020-01-06 22:11:10 +0100

    Added base Korisnik class.

---
+++ IB130149/Models/Korisnik.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IB130149.Models
+{
+    public class Korisnik
+    {
+        [Key]
+        public int Id { get; set; }
+        public string KorisnickoIme { get; set; }
+        public string Lozinka { get; set; }
+
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+
+        public string Adresa { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefon { get; set; }
+
+        public bool Klijent { get; set; }
+
+        public List<Uposlenik> Uposlenja { get; set; }
+    }
+}
