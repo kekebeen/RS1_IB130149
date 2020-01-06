8cee31351915d54a73ded1785074c063ad927c4c
unknown <kakosi69@gmail.com>
2020-01-06 22:10:56 +0100

    Added db context class

---
+++ IB130149/Context/Mycontext.cs
@@ -0,0 +1,31 @@
+﻿using IB130149.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IB130149.Context
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> x) : base(x)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //modelBuilder.Entity<Odjeljenje>()
+            //    .HasOne(x => x.Razrednik)
+            //    .WithMany()
+            //    .HasForeignKey(x => x.RazrednikID)
+            //    .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        public DbSet<Korisnik> Korisnik { get; set; }
+        public DbSet<Uposlenik> Uposlenik { get; set; }
+    }
+}
