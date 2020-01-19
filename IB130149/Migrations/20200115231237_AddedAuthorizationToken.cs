d6a24f5bd73a0520f1daecd0096fd4693c4b88ab
kekebeen <kakosi69@gmail.com>
2020-01-19 13:55:30 +0100

    Updates on the migrations.

---
+++ IB130149/Migrations/20200115231237_AddedAuthorizationToken.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IB130149.Migrations
+{
+    public partial class AddedAuthorizationToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthorizationToken",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorizationToken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AuthorizationToken_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorizationToken_UserId",
+                table: "AuthorizationToken",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorizationToken");
+        }
+    }
+}
---
+++ IB130149/Migrations/20200117212106_UpdatedVirtualConstraint.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IB130149.Migrations
+{
+    public partial class UpdatedVirtualConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
