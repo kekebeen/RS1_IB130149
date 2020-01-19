32582475466bbc9232440682728887d38e210f1d
kekebeen <kakosi69@gmail.com>
2020-01-19 13:56:21 +0100

    Added login viewmodel.

---
+++ IB130149/ViewModels/LoginVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IB130149.ViewModels
+{
+    public class LoginVM
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100, ErrorMessage = "Username field must contain at least 3 characters.", MinimumLength = 3)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "Password field must contain at least 4 characters.", MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
