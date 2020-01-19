427ead750e726931687e92edb35cb27c2ee3c39c
kekebeen <kakosi69@gmail.com>
2020-01-19 13:54:53 +0100

    Added base areas for the project with implicit controllers and views.

---
+++ IB130149/Areas/Client/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IB130149.Helper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IB130149.Areas.Client.Controllers
+{
+    [Area(nameof(Client))]
+    [Route("portal/client/[controller]")]
+    [Authorization(isAdmin: true, isRepairman: false, isSeller: false, isClient: true)]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IB130149/Areas/Repairman/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IB130149.Helper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IB130149.Areas.Repairman.Controllers
+{
+    [Area(nameof(Repairman))]
+    [Route("portal/service/[controller]")]
+    [Authorization(isAdmin: false, isRepairman: true, isSeller: false, isClient: false)]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
