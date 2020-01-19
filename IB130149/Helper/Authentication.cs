e8234bbde034f0eafe849cb181bdf16082a6df73
kekebeen <kakosi69@gmail.com>
2020-01-19 13:55:17 +0100

    Auth logic with cookie/session support filters.

---
+++ IB130149/Helper/Authentication.cs
@@ -0,0 +1,92 @@
+﻿using IB130149.Models;
+using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using IB130149.Context;
+
+
+namespace IB130149.Helper
+{
+    public static class Authentication
+    {
+        private const string loggedInUser = "logged_in_user";
+
+
+        public static void SetLoggedUser(this HttpContext context, User user, bool rememberMe=false)
+        {
+            MyContext db = context.RequestServices.GetService<MyContext>();
+            string oldToken = context.Request.GetCookieJson<string>(loggedInUser);
+            
+            if(oldToken != null)
+            {
+                AuthorizationToken toDelete = db.AuthorizationToken.FirstOrDefault(x => x.Value == oldToken);
+                
+                if(toDelete != null)
+                {
+                    db.SaveChanges();
+                }
+            }
+
+            if(user != null)
+            {
+                string token = Guid.NewGuid().ToString();
+                db.AuthorizationToken.Add(new AuthorizationToken
+                {
+                    Value = token,
+                    UserId = user.Id
+                });
+                db.SaveChanges();
+                context.Response.SetCookieJson(loggedInUser, token);
+            }
+        }
+    
+        public static string GetCurrentToken(this HttpContext context)
+        {
+            return context.Request.GetCookieJson<string>(loggedInUser);
+        }
+
+        public static User GetLoggedInuser(this HttpContext context)
+        {
+            MyContext db = context.RequestServices.GetService<MyContext>();
+
+            string token = context.Request.GetCookieJson<string>(loggedInUser);
+
+            if(token == null)
+            {
+                return null;
+            }
+
+            return db.AuthorizationToken
+                .Where(x => x.Value == token)
+                .Select(s => new User { 
+                    Name = s.User.Name,
+                    Surname = s.User.Surname,
+                    Username = s.User.Username,
+                    Email = s.User.Email,
+                    Password = s.User.Password,
+                    Address = s.User.Address,
+                    Telephone = s.User.Telephone,
+                    Employee = db.Employee.Where(x => x.UserId == s.UserId).ToList()
+                })
+                .SingleOrDefault();
+        }
+
+        public static void Logout(this HttpContext context)
+        {
+            MyContext db = context.RequestServices.GetService<MyContext>();
+            string token = context.Request.GetCookieJson<string>(loggedInUser);
+
+            AuthorizationToken current = db.AuthorizationToken.Where(x => x.Value == token).SingleOrDefault();
+            if(current != null)
+            {
+                db.AuthorizationToken.Remove(current);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+ 
---
+++ IB130149/Helper/Authorization.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using IB130149.Models;
+using IB130149.Context;
+
+namespace IB130149.Helper
+{
+    public class AuthorizationAttribute : TypeFilterAttribute
+    {
+        public AuthorizationAttribute(bool isAdmin, bool isRepairman, bool isSeller, bool isClient) : base (typeof(MyAuthorizeImpl))
+        {
+            Arguments = new object[] { isAdmin, isRepairman, isSeller, isClient };
+        }
+    }
+
+    public class MyAuthorizeImpl: IAsyncActionFilter
+    {
+        public MyAuthorizeImpl(bool admin, bool repairman, bool seller, bool client)
+        {
+            _admin = admin;
+            _repairman = repairman;
+            _seller = seller;
+            _client = client;
+        }
+
+        private readonly bool _admin;
+        private readonly bool _repairman;
+        private readonly bool _seller;
+        private readonly bool _client;
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext filterContext, ActionExecutionDelegate next)
+        {
+            User user = filterContext.HttpContext.GetLoggedInuser();
+
+            if (user == null)
+            {
+                if (filterContext.Controller is Controller controller)
+                {
+                    controller.TempData["error_message"] = "You are not logged in.";
+                }
+
+                filterContext.Result = new RedirectToActionResult("Signin", "Home", new { area = "" });
+                return;
+            }
+
+            MyContext db = filterContext.HttpContext.RequestServices.GetService<MyContext>();
+
+            if (user.Employee != null)
+            {
+                // admin can access
+                if (_admin && user.Employee.Any(x => (bool)x.isAdmin))
+                {
+                    await next();
+                    filterContext.Result = new RedirectToActionResult("Index", "Home", new { Area = "Admin" });
+                    return;
+                }
+                // repairman can access
+                if (_repairman && user.Employee.Any(x => (bool)x.isRepairman))
+                {
+                    await next();
+                    filterContext.Result = new RedirectToActionResult("Index", "Home", new { Area = "Repairman" });
+                    return;
+                }
+                // seller can access
+                if (_seller && user.Employee.Any(x => (bool)x.isSeller))
+                {
+                    await next();
+                    filterContext.Result = new RedirectToActionResult("Index", "Home", new { Area = "Seller" });
+                    return;
+                }
+            }
+            // client can access
+            if (_client && user.isClient)
+            {
+                await next();
+                filterContext.Result = new RedirectToActionResult("Index", "Home", new { Area = "Client" });
+                return;
+            }
+
+            if(filterContext.Controller is Controller c1)
+            {
+                c1.ViewData["error_message"] = "You dont have access to this resource.";
+            }
+            filterContext.Result = new RedirectToActionResult("Signin", "Home", new { @area = "" });
+        }
+
+        public void OnActionExecuted(ActionResult context) { }
+    }
+}
