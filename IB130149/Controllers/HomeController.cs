3833af077702c444cf2780924dcdf024823433db
kekebeen <kakosi69@gmail.com>
2020-01-19 13:51:55 +0100

    Added initial role routing logic in HomeController.

--- IB130149/Controllers/HomeController.cs
+++ IB130149/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using IB130149.Models;
 using IB130149.Context;
 using IB130149.Helper;
+using IB130149.ViewModels;
 
 namespace IB130149.Controllers
 {
@@ -21,19 +22,126 @@ namespace IB130149.Controllers
             _logger = logger;
             _context = db;
         }
-        [Authorization(isAdmin: true, isRepairman: true, isSeller: true, isClient: true)]
+
         public IActionResult Index()
         {
-            //TODO: handle redirect logic here once auth is ready
-            //Redirect to area controllers based on employee role
-            List<User> users = _context.User.ToList();
-            ViewBag.users = users;
-            return View();
+            // On initial route catch, check logged in user role and redirect accordingly to the areas.
+            User user = HttpContext.GetLoggedInuser();
+            
+            if(user == null)
+            {
+                TempData["error_message"] = "You don't have access rights. Please login below.";
+                return RedirectToAction("Signin", "Home");
+            }
+            // Check user roles and assign him to the area
+            // Client role
+            if(user.isClient)
+            {
+                return RedirectToAction("Index", "Home", new { Area = "Client" });
+            }
+
+            if(user.Employee != null)
+            {
+                if(user.Employee.Any(x => (bool)x.isAdmin))
+                {
+                    return RedirectToAction("Index", "Home", new { Area = "Admin" });
+                }
+
+                if (user.Employee.Any(x => (bool)x.isRepairman))
+                {
+                    return RedirectToAction("Index", "Home", new { Area = "Repairman" });
+                }
+
+                if (user.Employee.Any(x => (bool)x.isSeller))
+                {
+                    return RedirectToAction("Index", "Home", new { Area = "Seller" });
+                }
+
+            }
+            return null;
         }
 
-        public IActionResult Privacy()
+        public IActionResult Signin(LoginVM? model)
         {
-            return View();
+            return View(model);
+        }
+
+        public IActionResult Login(LoginVM model)
+        {
+            User user = _context.User.SingleOrDefault(x => x.Email == model.Email && x.Password == model.Password);
+
+            if (user == null)
+            {
+                TempData["error_message"] = "Wrong username or password.";
+                return View("Signin", model);
+            }
+
+            HttpContext.SetLoggedUser(user, model.RememberMe);
+            // Home index action will take care of the role redirection for the current user role.
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Signup(SignupVm? model)
+        {
+            TempData["error_message"] = (String)TempData["error_message"];
+            return View(model);
+        }
+
+        public IActionResult Register(SignupVm model)
+        {
+            // check if model state is ok
+            if(!ModelState.IsValid)
+            {
+                TempData["error_message"] = "Oops. Please check your data.";
+                return View("Signup");
+            }
+            // check if username/email already exists in db
+            User user = _context.User.Where(x => x.Email == model.Email).SingleOrDefault();
+            
+            if(user != null)
+            {
+                TempData["error_message"] = "User with given email address already exists. Please try something else.";
+                return View("Signup",model);
+            }
+
+            // create new user object
+             User newUser = new User
+            {
+                Username = model.Username,
+                Password = model.Password,
+                Name = model.Name,
+                Surname = model.Surname,
+                Email = model.Email,
+                Address = model.Address,
+                Telephone = model.Telephone,
+                isClient = true
+            };
+
+            LoginVM loginViewModel = new LoginVM
+            {
+                Email = model.Email,
+                Password = model.Password,
+                RememberMe = false
+            };
+
+            // save to db
+            _context.User.Add(newUser);
+            _context.SaveChanges();
+            // Use TempData and store temporarily loginVM model from the user object
+            // Pass it down to the login view and prepolate the form.
+            TempData["success_message"] = "You have successfully created new user. Please login below.";
+            return View("Signin", loginViewModel);
+
+        }
+
+        public IActionResult Logout()
+        {
+            // Authorization helper handles cookie expiration date;
+            // In case there is no RememberMe flag just delete from AuthorizationToken db;
+            HttpContext.Logout();
+            TempData["success_message"] = "You have successfully been logged out.";
+            return RedirectToAction("Signin", "Home");
+            // In case there is expiration, it should expire on its own(no helper needed here);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
