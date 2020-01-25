c3330921c6b303e18b8e9108e9833cb7b85e734d
kekebeen <kakosi69@gmail.com>
2020-01-25 14:31:38 +0100

    Updates on the client viewmodels.

---
+++ IB130149/Areas/Client/ViewModels/ClientIndexVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IB130149.Areas.Client.ViewModels
+{
+    public class ClientIndexVm
+    {
+        // initial client service request list
+        // it consists of client list of rows
+        public string? DeliveryAddress { get; set; } // latest request note
+        public string? Status { get; set; } // latest request status
+        public List<ServiceRequestVm> ClientServiceRequests { get; set; }
+    }
+}
---
+++ IB130149/Areas/Client/ViewModels/ServiceRequestCreateVm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IB130149.Areas.Client.ViewModels
+{
+
+    public class ServiceRequestCreateVm
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "Address field must have at least three characters.", MinimumLength = 3)]
+        public string Address { get; set; }
+        [Required]
+        public DateTime RequestDate { get; set; }
+        [Required]
+        public int RequestedById { get; set; }
+        public string? Impression { get; set; }
+        [DefaultValue(false)]
+        public bool IncludeHomeService { get; set; }
+        [DefaultValue(false)]
+        public bool IncludeDelivery { get; set; }
+        [DefaultValue(false)]
+        public bool IncludeCustomerPickup { get; set; }
+    }
+}
