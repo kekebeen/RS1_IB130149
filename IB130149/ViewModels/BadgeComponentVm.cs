80b34383a0a0f0d6a4d33a9a1bbc831d4dbe9ae8
kekebeen <kakosi69@gmail.com>
2020-01-25 14:33:01 +0100

    Added Badge component viewmodel.

---
+++ IB130149/ViewModels/BadgeComponentVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IB130149.ViewModels
+{
+    public class BadgeComponentVm
+    {
+        public string status { get; set; }
+    }
+}
