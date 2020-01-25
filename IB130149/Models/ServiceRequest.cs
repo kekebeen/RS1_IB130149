9af1bf13db89ac4ea615e29da053cf16a004634f
kekebeen <kakosi69@gmail.com>
2020-01-25 14:32:34 +0100

    Added models.

---
+++ IB130149/Models/ServiceRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IB130149.Models
+{
+    public class ServiceRequest
+    {
+        public int Id { get; set; }
+        public DateTime RequestDate { get; set; }
+        public string DeliveryAddress { get; set; }
+        public int RequestedById { get; set; }
+        [ForeignKey(nameof(RequestedById))]
+        public virtual User Client { get; set; }
+        public string? Impression { get; set; }
+        public bool IncludeHomeService { get; set; }
+        public bool IncludeDelivery { get; set; }
+        public bool IncludeCustomerPickup { get; set; }
+    }
+}
---
+++ IB130149/Models/ServiceTicket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IB130149.Models
+{
+    public class ServiceTicket
+    {
+        public int Id { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime? RequestDoneDate { get; set; }
+        public int StatusId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
+        public int CustomerId { get; set; }
+        public virtual User Customer { get; set; }
+        [ForeignKey(nameof(EmployeeId))]
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+        [ForeignKey(nameof(ServiceRequestId))]
+        public int ServiceRequestId { get; set; }
+        public virtual ServiceRequest ServiceRequest { get; set; }
+        public string Note { get; set; }
+    }
+}
